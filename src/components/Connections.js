1b5c9ea62798b36e8f04b72d6bda94324195cfa1
Griffin McCauley <griffin_mccauley@brown.edu>
2021-08-13 01:41:11 -0600

    Added other placeholder view components

---
+++ src/components/Connections.js
@@ -0,0 +1,104 @@
+// Setup
+import React from "react";
+
+// MUI Stuff
+import Grid from "@material-ui/core/Grid";
+
+import Button from "@material-ui/core/Button";
+import SearchBar from "material-ui-search-bar";
+import InputLabel from "@material-ui/core/InputLabel";
+import CardContent from "@material-ui/core/CardContent";
+import MenuItem from "@material-ui/core/MenuItem";
+import Card from "@material-ui/core/Card";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Typography from "@material-ui/core/Typography";
+
+function Connections() {
+  return (
+    <div>
+        Pending Connections
+        <GridList cols={5} spacing={10} cellHeight="auto">
+        {[0, 1, 2, 3, 4].map((index) => {
+          return (
+            <GridListTile item component="Card" sm>
+              <Card align="center">
+                <CardContent>
+                  <img
+                    width="50px"
+                    alt="Profile Picture"
+                    src={
+                      "https://firebasestorage.googleapis.com/v0/b/uconnect-5eebd.appspot.com/o/no-img.png?alt=media&token=6aa9ff82-c1a6-4e1f-a743-67a828790599"
+                    }
+                  />
+                  <Typography variant="body2">Jane Smith '23</Typography>
+                </CardContent>
+              </Card>
+            </GridListTile>
+          );
+        })}
+      </GridList>
+      All Connections
+      <SearchBar />
+      <Grid container spacing={10}>
+        <Grid item>
+          <FormControl style={{ minWidth: 120 }}>
+            <InputLabel>Class Year</InputLabel>
+            <Select>
+              <MenuItem value="2022">2022</MenuItem>
+              <MenuItem value="2023">2023</MenuItem>
+              <MenuItem value="2024">2024</MenuItem>
+              <MenuItem value="2025">2025</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl style={{ minWidth: 120 }}>
+            <InputLabel>Interests</InputLabel>
+            <Select>
+              <MenuItem value="2022">2022</MenuItem>
+              <MenuItem value="2023">2023</MenuItem>
+              <MenuItem value="2024">2024</MenuItem>
+              <MenuItem value="2025">2025</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl style={{ minWidth: 120 }}>
+            <InputLabel>Concentration</InputLabel>
+            <Select>
+              <MenuItem value="2022">2022</MenuItem>
+              <MenuItem value="2023">2023</MenuItem>
+              <MenuItem value="2024">2024</MenuItem>
+              <MenuItem value="2025">2025</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <GridList cols={5} spacing={10} cellHeight="auto">
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => {
+          return (
+            <GridListTile item component="Card" sm>
+              <Card align="center">
+                <CardContent>
+                  <img
+                    width="50px"
+                    alt="Profile Picture"
+                    src={
+                      "https://firebasestorage.googleapis.com/v0/b/uconnect-5eebd.appspot.com/o/no-img.png?alt=media&token=6aa9ff82-c1a6-4e1f-a743-67a828790599"
+                    }
+                  />
+                  <Typography variant="body2">Jane Smith '23</Typography>
+                </CardContent>
+              </Card>
+            </GridListTile>
+          );
+        })}
+      </GridList>
+    </div>
+  );
+}
+
+export default Connections;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,99 @@
 // Setup
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 // Components
 import NavBar from "../components/NavBar";
 import Landing from "../components/Landing";
+import Connections from "../components/Connections";
+import Messages from "../components/Messages";
+import Course from "../components/Course";
+import Notifications from "../components/Notifications";
 
 // MUI Stuff
 import Grid from "@material-ui/core/Grid";
-
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 // Body
 function Home() {
+  const [page, setPage] = useState("Home");
+  const [courseOpen, setCourseOpen] = useState(false);
   return (
     <div align="center">
       <NavBar />
       <Grid container spacing={3}>
         <Grid item xs={3}>
-          <Button fullWidth variant="contained">
+          <Button fullWidth variant="contained" onClick={() => setPage("Home")}>
             Home
           </Button>
-          <Button fullWidth variant="contained">
+          <Button fullWidth variant="contained" onClick={() => setPage("Messages")}>
             Messages
           </Button>
-          <Button fullWidth variant="contained">
+          <Button fullWidth variant="contained" onClick={() => setPage("Connections")}>
             Connections
           </Button>
+          <Button fullWidth variant="contained" onClick={() => setCourseOpen(true)}>
+            Courses
+          </Button>
+          {courseOpen && (
+            <div>
+              <Button fullWidth variant="contained" onClick={() => (setPage("courseOne"), setCourseOpen(false))}>
+            CODE 0001
+          </Button>
+          <Button fullWidth variant="contained" onClick={() => (setPage("courseTwo"), setCourseOpen(false))}>
+            CODE 0002
+          </Button>
+          <Button fullWidth variant="contained" onClick={() => (setPage("courseThree"), setCourseOpen(false))}>
+            CODE 0003
+          </Button>
+          <Button fullWidth variant="contained" onClick={() => (setPage("courseFour"), setCourseOpen(false))}>
+            CODE 0004
+          </Button>
+            </div>
+          )}
+
+          {/* <FormControl fullWidth>
+            <InputLabel>Courses</InputLabel>
+            <Select>
+              <MenuItem value="CODE0001">
+                <Button onClick={() => setPage("courseOne")}>CODE 0001</Button>
+              </MenuItem>
+              <MenuItem value="CODE0002">
+                <Button onClick={() => setPage("courseTwo")}>CODE 0002</Button>
+              </MenuItem>
+              <MenuItem value="CODE0003">
+                <Button onClick={() => setPage("courseThree")}>CODE 0003</Button>
+              </MenuItem>
+              <MenuItem value="CODE0004">
+                <Button onClick={() => setPage("courseFour")}>CODE 0004</Button>
+              </MenuItem>
+            </Select>
+          </FormControl> */}
+
+          {page !== "Home" && (
+            <Notifications />
+          )}
         </Grid>
         <Grid item xs={9} align="left">
-          <Landing />
+          {page === "Home" && (
+            <div>
+            <Landing />
+            <Notifications />
+            </div>
+          )}
+          {page === "Messages" && (
+            <Typography>Messages</Typography>
+          )}
+          {page === "Connections" && (
+            <Connections />
+          )}
+          {(page === "courseOne" || page === "courseTwo" || page === "courseThree" || page === "courseFour") && (
+            <Course />
+          )}
         </Grid>
       </Grid>
     </div>
