195730e4059eb75f3e515ac49fb49c6705a75a9b
Melissa Zhang <mzhang19096@gmail.com>
2021-09-06 13:53:02 -0400

    restore message

--- src/pages/ConnectionsView.js
+++ src/pages/ConnectionsView.js
@@ -6,7 +6,7 @@ import { auth } from "../firebase";
 // Components
 import StudentModal from "../components/StudentModal";
 import SearchBar from "../components/SearchBar";
-import Message from "../pages/MessageView";
+import Message from "../components/Message";
 
 import { Container, Row, Modal } from "react-bootstrap";
 
