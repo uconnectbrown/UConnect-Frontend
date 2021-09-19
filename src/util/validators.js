20cde91bddf366ca2c3a622848478db963fa11e1
Griffin McCauley <griffin_mccauley@brown.edu>
2021-09-18 20:57:07 -0400

    remove image dependency for saving changes

--- src/util/validators.js
+++ src/util/validators.js
@@ -6,9 +6,9 @@ exports.validProfile = (profile) => {
     profile.majors.filter(Boolean).length > 0 &&
     profile.interests1.length === 3 &&
     profile.interests2.length === 3 &&
-    profile.interests3.length === 3 &&
-    profile.imageUrl !==
-      "https://firebasestorage.googleapis.com/v0/b/uconnect-5eebd.appspot.com/o/no-img.png?alt=media"
+    profile.interests3.length === 3
+    // profile.imageUrl !==
+    //   "https://firebasestorage.googleapis.com/v0/b/uconnect-5eebd.appspot.com/o/no-img.png?alt=media"
   ) {
     return true;
   }
