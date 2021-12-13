c031b0166e7a13f064d782c999d7c9791deac34f
Nicholas Bottone <nick@bottone.io>
2021-12-13 18:57:51 -0500

    Begin adding styling and links to post pages

--- src/pages/HomeView.js
+++ src/pages/HomeView.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Card, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import "./HomeView.css";
 
 export default function HomeView() {
   const MyCard = (props) => (
-    <Card>
-      <Card.Title>Post ###</Card.Title>
+    <Card className="PostCard" as={Link} to={`/post/${props.postNumber}`}>
+      <Card.Title>Post {props.postNumber}</Card.Title>
       <Card.Body>
         <Card.Text>
           Bacon ipsum dolor amet ball tip cupim alcatra meatball kevin jerky.
@@ -16,8 +17,12 @@ export default function HomeView() {
           buffalo chislic swine strip steak ball tip jerky beef pastrami
           chicken. Cow ball tip jowl pig strip steak rump.
         </Card.Text>
+        {/* Like button with count of number of reactions */}
+        <Button variant="secondary" size="sm">
+          Like
+        </Button>
       </Card.Body>
-      <Card.Footer>
+      <Card.Footer className="PostFooter">
         <small className="text-muted">3 mins ago</small>
       </Card.Footer>
     </Card>
@@ -25,12 +30,21 @@ export default function HomeView() {
 
   return (
     <div className="HomeView">
-      <h1>Home</h1>
-      <div class="p-5 mb-4 bg-light rounded-3">
-        <Container fluid py={5}>
-          <Row xs={1} md={2} lg={3}>
+      <Row>
+        <Col sm={10}>
+          <h1>Home</h1>
+        </Col>
+        <Col sm={2}>
+          <Button as={Link} variant="primary" to="/post/create">
+            Create Post
+          </Button>
+        </Col>
+      </Row>
+      <div class="p-3 mb-4 bg-light rounded-3">
+        <Container fluid py={5} className="FeedContainer">
+          <Row sm={1} lg={2} xl={3}>
             {Array.from({ length: 10 }).map((_, idx) => (
-              <MyCard key={idx} />
+              <MyCard key={idx} postNumber={idx} />
             ))}
           </Row>
         </Container>
