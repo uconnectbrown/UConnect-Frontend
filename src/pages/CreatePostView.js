4fcaa23bc3e92b1b7a27349bf9d7efe2ad062931
Nicholas Bottone <nick@bottone.io>
2022-01-12 02:44:31 -0500

    Improve pseudonym toggle for post form

--- src/pages/CreatePostView.js
+++ src/pages/CreatePostView.js
@@ -2,15 +2,34 @@ import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
 
 export default function CreatePostView() {
+  const [posterMode, setPosterMode] = React.useState(true);
+  const [poster, setPoster] = React.useState("Me");
+  const [postText, setPostText] = React.useState("");
+
   return (
     <div className="CreatePostView">
       <h1>Create Post</h1>
       <div class="p-3 mb-4 bg-light rounded-3">
         <Container fluid py={5} className="PostFormContainer">
           <Form>
-            <Form.Group controlId="poster">
+            <Form.Group controlId="posterMode">
               <Form.Label>Poster</Form.Label>
-              <Form.Control type="text" placeholder="Enter poster name" />
+              <Form.Check
+                type="switch"
+                label={posterMode ? "Post as Me" : "Post as Pseudonym"}
+                name="posterMode"
+                checked={posterMode}
+                onChange={() => {
+                  setPosterMode(!posterMode);
+                  !posterMode ? setPoster("Me") : setPoster("");
+                }}
+              />
+              <Form.Control
+                type="text"
+                placeholder="Enter poster name"
+                value={poster}
+                disabled={posterMode}
+              />
             </Form.Group>
             <Form.Group controlId="postText">
               <Form.Label>Post Text</Form.Label>
@@ -18,6 +37,8 @@ export default function CreatePostView() {
                 as="textarea"
                 rows="3"
                 placeholder="Enter post body"
+                value={postText}
+                onChange={(e) => setPostText(e.target.value)}
               />
             </Form.Group>
             <Button variant="primary" type="submit">
