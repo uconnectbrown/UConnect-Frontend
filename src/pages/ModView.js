2abdac67fdcc50dcfb1d8af13f3fa5a0f8d8d658
Nicholas Bottone <nick@bottone.io>
2022-03-02 18:04:43 -0500

    Renovate from posts to events

--- src/pages/ModView.js
+++ src/pages/ModView.js
@@ -1,20 +1,18 @@
 import React from "react";
-import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Button, Card, Container, Image } from "react-bootstrap";
 
-import PostCard from "../components/PostCard";
-import { getModFeedPosts } from "../util/postingBoardUtil";
+import { getModFeedEvents } from "../util/eventBoardUtil";
 import "./ModView.css";
 
 export default function ModView() {
-  const [posts, setPosts] = React.useState([]);
+  const [events, setEvents] = React.useState([]);
 
   React.useEffect(() => {
-    async function fetchPosts() {
-      const postsRes = await getModFeedPosts();
-      setPosts(postsRes || []);
+    async function fetchEvents() {
+      const eventsRes = await getModFeedEvents();
+      setEvents(eventsRes || []);
     }
-    fetchPosts();
+    fetchEvents();
   }, []);
 
   return (
@@ -22,19 +20,28 @@ export default function ModView() {
       <h1>Moderator Portal</h1>
       <div class="p-3 mb-4 rounded-3">
         <Container fluid py={5} className="FeedContainer">
-          {posts.map((post) => (
+          {events.map((event) => (
             <Card className="ModCard">
               <Card.Header>
                 <Image
-                  src={post.author.avatar}
+                  src={event.author.avatar}
                   alt="avatar"
                   roundedCircle
                   width={32}
                   height={32}
                 />
-                <b> {post.author.displayName}</b>
+                <b> {event.author.displayName}</b>
               </Card.Header>
-              <Card.Body>{post.postText}</Card.Body>
+              <Card.Body>
+                <p>{event.eventTitle}</p>
+                <p>{event.eventDescription}</p>
+                <p>{event.eventLocation}</p>
+                <p>{new Date(event.eventDate).toLocaleString()}</p>
+                <p>
+                  Submitted by: {event.author.displayName}, Hosted By:{" "}
+                  {event.hostedBy}
+                </p>
+              </Card.Body>
               <Card.Footer>
                 <Button variant="success">Approve</Button>
                 <Button variant="danger">Reject</Button>
